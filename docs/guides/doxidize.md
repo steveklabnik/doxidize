8c2d7cef822db57d809282b7f860404789dda2ef
steveklabnik <steve@steveklabnik.com>
2018-01-19 18:05:58 -0500

    move api doc generation into generate

--- docs/guides/doxidize.md
+++ docs/guides/doxidize.md
@@ -7,3 +7,14 @@ $ doxidize
 ```
 
 It takes no parameters.
+
+## Generated files
+
+`doxidize` will create a directory at the top-level of your project, `docs`.
+It will fill it with these files:
+
+* `README.md`: the home page of your documentation
+* An `api` directory
+* A markdown file inside of the `api` directory for each item in your crate.
+  These files will be pre-filled with any doc comments you've written in your
+  source.
--- src/lib.rs
+++ src/lib.rs
@@ -58,17 +58,7 @@ pub fn create_skeleton(dir: &Path) -> Result<()> {
     let readme = docs_dir.join("Menu.toml");
     OpenOptions::new().create(true).append(true).open(readme)?;
 
-    Ok(())
-}
-
-pub fn generate(dir: &Path) -> Result<()> {
-    // we need to know where the docs are
-    let docs_dir = dir.join("docs");
-
-    // ensure that the docs dir exists in target
-    let target_dir = dir.join("target").join("docs").join("public");
-    fs::create_dir_all(&target_dir)?;
-
+    // now the api docs
     let mut handlebars = Handlebars::new();
 
     handlebars.register_template_file("page", "templates/page.hbs")?;
@@ -82,59 +72,8 @@ pub fn generate(dir: &Path) -> Result<()> {
             }
 
             Ok(())
-      }));
+    }));
 
-
-    // render all other *.md files as *.html, walking the tree
-    for entry in WalkDir::new(&docs_dir) {
-        let entry = entry?;
-        let path = entry.path();
-
-        // we want only files
-        if !path.is_file() { continue; }
-
-        if let Some(extension) = path.extension() {
-            // we only want .md files
-            if extension != "md" {
-                continue;
-            }
-        } else {
-            // we don't want files with no extension
-            continue;
-        }
-
-        // we certainly have a file name, since we're looping over real files
-        let file_name = path.file_name().unwrap();
-
-        // make sure the containing directory is created
-        //
-        // to do this, we get the containing directory, strip off the base, and then re-apply that path
-        // to where we want to write the output. tricky!
-        let containing_dir = path.parent().expect("somehow this is running at the root");
-        let new_containing_dir = containing_dir.strip_prefix(&docs_dir)?;
-        let new_containing_dir = target_dir.join(new_containing_dir);
-        fs::create_dir_all(&new_containing_dir)?;
-
-        // now we can make the file
-        let mut file = File::open(&path)?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-
-        let rendered_contents = comrak::markdown_to_html(&contents, &ComrakOptions::default());
-
-        let rendered_path = if file_name == "README.md" {
-            new_containing_dir.join("index.html")
-        } else {
-            new_containing_dir.join(file_name).with_extension("html")
-        };
-
-        let mut file = File::create(rendered_path)?;
-
-        // TODO: this only works at one level down; we need to calculate the real number
-        file.write_all(handlebars.render("page", &json!({"contents": rendered_contents, "nest-count": 1}))?.as_bytes())?;
-    }
-
-    // now it's time for api docs
 
     // ensure that the api dir exists
     let api_dir = docs_dir.join("api");
@@ -259,6 +198,83 @@ pub fn generate(dir: &Path) -> Result<()> {
         let mut file = File::create(markdown_path)?;
 
         file.write_all(handlebars.render("api", &json!({"name": def.name, "docs": def.docs}))?.as_bytes())?;
+    }
+
+
+    Ok(())
+}
+
+pub fn generate(dir: &Path) -> Result<()> {
+    // we need to know where the docs are
+    let docs_dir = dir.join("docs");
+
+    // ensure that the docs dir exists in target
+    let target_dir = dir.join("target").join("docs").join("public");
+    fs::create_dir_all(&target_dir)?;
+
+    let mut handlebars = Handlebars::new();
+
+    handlebars.register_template_file("page", "templates/page.hbs")?;
+    handlebars.register_template_file("api", "templates/api.hbs")?;
+    handlebars.register_helper("up-dir",
+        Box::new(|h: &handlebars::Helper, _: &Handlebars, rc: &mut handlebars::RenderContext| -> handlebars::HelperResult {
+            let count = h.param(0).map(|v| v.value().as_u64().unwrap()).unwrap();
+
+            for _ in 0..count {
+                rc.writer.write(b"../")?;
+            }
+
+            Ok(())
+      }));
+
+
+    // render all other *.md files as *.html, walking the tree
+    for entry in WalkDir::new(&docs_dir) {
+        let entry = entry?;
+        let path = entry.path();
+
+        // we want only files
+        if !path.is_file() { continue; }
+
+        if let Some(extension) = path.extension() {
+            // we only want .md files
+            if extension != "md" {
+                continue;
+            }
+        } else {
+            // we don't want files with no extension
+            continue;
+        }
+
+        // we certainly have a file name, since we're looping over real files
+        let file_name = path.file_name().unwrap();
+
+        // make sure the containing directory is created
+        //
+        // to do this, we get the containing directory, strip off the base, and then re-apply that path
+        // to where we want to write the output. tricky!
+        let containing_dir = path.parent().expect("somehow this is running at the root");
+        let new_containing_dir = containing_dir.strip_prefix(&docs_dir)?;
+        let new_containing_dir = target_dir.join(new_containing_dir);
+        fs::create_dir_all(&new_containing_dir)?;
+
+        // now we can make the file
+        let mut file = File::open(&path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        let rendered_contents = comrak::markdown_to_html(&contents, &ComrakOptions::default());
+
+        let rendered_path = if file_name == "README.md" {
+            new_containing_dir.join("index.html")
+        } else {
+            new_containing_dir.join(file_name).with_extension("html")
+        };
+
+        let mut file = File::create(rendered_path)?;
+
+        // TODO: this only works at one level down; we need to calculate the real number
+        file.write_all(handlebars.render("page", &json!({"contents": rendered_contents, "nest-count": 1}))?.as_bytes())?;
     }
 
     Ok(())
