c548c0f4a502ea3af51625c38b29d79c467e0354
steveklabnik <steve@steveklabnik.com>
2018-02-07 14:31:17 -0500

    this refeactoring went a bit too far

--- src/ops/init/mod.rs
+++ src/ops/init/mod.rs
@@ -1,9 +1,9 @@
 mod api;
-mod examples;
 
 use slog::Logger;
 
-use std::fs::{self, OpenOptions};
+use std::fs::{self, File, OpenOptions};
+use std::io::prelude::*;
 
 use Config;
 use Result;
@@ -20,7 +20,7 @@ pub fn init(config: &Config, log: &Logger) -> Result<()> {
     create_doxidize_config(config, &log)?;
     create_menu_toml(config, &log)?;
 
-    examples::create(config, &log)?;
+    create_examples(config, &log)?;
 
     api::create(config, &log)?;
 
@@ -65,6 +65,65 @@ fn create_menu_toml(config: &Config, log: &Logger) -> Result<()> {
 
     debug!(log, "creating Menu.toml"; o!("file" => menu.display()));
     OpenOptions::new().create(true).append(true).open(menu)?;
+
+    Ok(())
+}
+
+fn create_examples(config: &Config, log: &Logger) -> Result<()> {
+    let examples_dir = config.examples_markdown_path();
+    debug!(log, "creating examples dir";
+    o!("dir" => examples_dir.display()));
+    fs::create_dir_all(&examples_dir)?;
+
+    if config.examples_path().is_dir() {
+        for entry in fs::read_dir(config.examples_path())? {
+            let entry = entry?;
+            let path = entry.path();
+
+            // we want only files
+            if !path.is_file() {
+                continue;
+            }
+            trace!(log, "file is a file, continuing");
+
+            if let Some(extension) = path.extension() {
+                // we only want .rs files
+                if extension != "rs" {
+                    continue;
+                }
+            } else {
+                // we don't want files with no extension
+                continue;
+            }
+            trace!(log, "file is a rust file, continuing");
+
+            // we certainly have a file name, since we're looping over real files
+            let file_name = path.file_name().unwrap();
+            let rust_file = config.examples_path().join(file_name);
+
+            trace!(log, "reading file";
+            "file" => rust_file.display());
+            let mut file = File::open(&rust_file)?;
+            let mut code = String::new();
+            file.read_to_string(&mut code)?;
+
+            let markdown_path = examples_dir.join(file_name).with_extension("md");
+
+            trace!(log, "rendering to markdown";
+            "file" => path.display(), "file" => markdown_path.display());
+            let mut file = File::create(markdown_path)?;
+
+            file.write_all(
+                config
+                    .handlebars()
+                    .render(
+                        "example",
+                        &json!({"name": file_name.to_str().unwrap(), "code": code}),
+                    )?
+                    .as_bytes(),
+            )?;
+        }
+    }
 
     Ok(())
 }
